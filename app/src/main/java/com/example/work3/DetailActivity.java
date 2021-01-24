5ae151d3d3dc122a9095d37422424a414a820711
MagerramovBoris <magerramov.boris.s@gmail.com>
2021-01-24 19:18:29 +0700

    Пункт 5 и 6. Добавил класс DetailActivity с макетом detail_activity + дополнительный класс со сведениями о элементе списка

---
+++ app/src/main/java/com/example/work3/DetailActivity.java
@@ -0,0 +1,32 @@
+package com.example.work3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        // saveInstanceState не равен нулю, когда есть состояние фрагмента
+        // сохранено из предыдущих конфигураций этого действия
+        // (например, при повороте экрана из книжной в альбомную).
+        // В этом случае фрагмент будет автоматически добавлен заново
+        // в свой контейнер, поэтому нам не нужно добавлять его вручную.
+        if (savedInstanceState == null) {
+            // Создаем детали fragment и добавляем его
+            Bundle arguments = new Bundle();
+            arguments.putString(
+                    ItemDetailFragment.ARG_ITEM_ID,
+                    getIntent().getStringExtra(ItemDetailFragment.ARG_ITEM_ID)
+            );
+            ItemDetailFragment fragment = new ItemDetailFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.details, fragment)
+                    .commit();
+        }
+    }
+
+}
